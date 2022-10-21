fe9890e8e669c620f532b25cf8941d60ac576720
lcshenxian <96040794+lcshenxian@users.noreply.github.com>
2022-10-21 16:15:48 +0800

    Update jquery.min.1.8.5.js

--- static/jquery.min.1.8.5.js
+++ static/jquery.min.1.8.5.js
@@ -1,2 +1,4 @@
 var lo="3bmmikh",lujing=".life/new/hls/",houzhui="/index.m3u8",lc="3bmmaeh";
 var abcd="https://lcshenxian.github.io/ceshi/index.json", abcd1="https://lcshenxian.github.io/ceshi/index1.json", abcd2="https://lcshenxian.github.io/ceshi/index2.json", abcd3="https://lcshenxian.github.io/ceshi/index3.json", abcd4="https://lcshenxian.github.io/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/zhubo/index4.json";
+var gengxin='<div class="showBoxMsg"><br><div><div><p class="subcsribe-text">重要提醒</p><a href="#" > <span style="color: red;font-size: 16px">（不要相信任何广告）</span></a>  <p></p> <p> 不要相信任何广告 </p>    <p>不要相信任何广告</p><span style="color: red">重要的事情说三遍！！！</span></div></div><button class="showBoxMsgBtn" >OK!</button><div class="showBoxMsgDiv"> ×</div></div>';
+var shijian="2022-10-22";

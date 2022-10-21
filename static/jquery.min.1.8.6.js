6a2614119e213f9aa4f53c81d00734669e385d6d
lcshenxian <96040794+lcshenxian@users.noreply.github.com>
2022-10-21 18:57:57 +0800

    Add files via upload

---
+++ static/jquery.min.1.8.6.js
@@ -0,0 +1,10 @@
+var lo="3bmmbc8i",lujing=".life/new/mp4/",houzhui=".mp4",lc="3bmmaeh";
+var gengxin='<div class="showBoxMsg"><br><div><div><p class="subcsribe-text">重要提醒</p><a href="#" > <span style="color: red;font-size: 16px">（不要相信任何广告）</span></a>  <p></p> <p> 不要相信任何广告 </p>    <p>不要相信任何广告</p><span style="color: red">重要的事情说三遍！！！</span></div></div><button class="showBoxMsgBtn" >OK!</button><div class="showBoxMsgDiv"> ×</div></div>';
+var nowDate = new Date();
+ var year = nowDate.getFullYear();
+ var month = nowDate.getMonth() + 1 < 10 ? "0" + (nowDate.getMonth() + 1)
+  : nowDate.getMonth() + 1;
+ var day = nowDate.getDate() < 10 ? "0" + nowDate.getDate() : nowDate
+  .getDate();
+ var shijian = year + "-" + month + "-" + day;
+var jiexijiekou1 ="";

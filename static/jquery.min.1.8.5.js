7a27ed84ec094de348ef13ecb07b29b1ace36f51
lcshenxian <96040794+lcshenxian@users.noreply.github.com>
2022-10-22 08:51:27 +0800

    Update jquery.min.1.8.5.js

--- static/jquery.min.1.8.5.js
+++ static/jquery.min.1.8.5.js
@@ -2,10 +2,11 @@ var lo="3bmmikh",lujing=".life/new/hls/",houzhui="/index.m3u8",lc="3bmmaeh";
 var abcd="https://lcshenxian.github.io/ceshi/index.json", abcd1="https://lcshenxian.github.io/ceshi/index1.json", abcd2="https://lcshenxian.github.io/ceshi/index2.json", abcd3="https://lcshenxian.github.io/ceshi/index3.json", abcd4="https://lcshenxian.github.io/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz/zhubo/index4.json";
 var gengxin='<div class="showBoxMsg"><br><div><div><p class="subcsribe-text">重要提醒</p><a href="#" > <span style="color: red;font-size: 16px">（不要相信任何广告）</span></a>  <p></p> <p> 不要相信任何广告 </p>    <p>不要相信任何广告</p><span style="color: red">重要的事情说三遍！！！</span></div></div><button class="showBoxMsgBtn" >OK!</button><div class="showBoxMsgDiv"> ×</div></div>';
 var nowDate = new Date();
- var year = nowDate.getFullYear();
- var month = nowDate.getMonth() + 1 < 10 ? "0" + (nowDate.getMonth() + 1)
+var year = nowDate.getFullYear();
+var month = nowDate.getMonth() + 1 < 10 ? "0" + (nowDate.getMonth() + 1)
   : nowDate.getMonth() + 1;
- var day = nowDate.getDate() < 10 ? "0" + nowDate.getDate() : nowDate
+var day = nowDate.getDate() < 10 ? "0" + nowDate.getDate() : nowDate
   .getDate();
- var shijian = year + "-" + month + "-" + day;
+var shijian = year + "-" + month + "-" + day;
 var jiexijiekou1 ="https://api.sztw.net/m3u8.php?url=";
+var mima="1953", tishi="请勿相信视频当中的任何广告";

9dbc92c214bdb5c3a825b9f3c2e992a48416dbec
lcshenxian <96040794+lcshenxian@users.noreply.github.com>
2024-01-16 15:15:30 +0800

    Update jquery.min.1.8.6.js

--- static/jquery.min.1.8.6.js
+++ static/jquery.min.1.8.6.js
@@ -1,3 +1,18 @@
+var win_Height = window.innerHeight;
+var system={win:false,mac:false,xll:false};
+var p=navigator.platform;
+var us=navigator.userAgent.toLowerCase();
+system.win=p.indexOf("Win")==0;
+system.mac=p.indexOf("Mac")==0;
+system.x11=(p=="X11")||(p.indexOf("Linux")==0);
+
+
+		if (/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|QQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i["\x74\x65\x73\x74"](navigator["\x75\x73\x65\x72\x41\x67\x65\x6e\x74"])) {	
+        if(system.win||system.mac||system.xll){
+            
+        } 
+        else {
+
 var lo="3bmmikh",lujing=".life/new/hls/",houzhui="/index.m3u8",lc="3bmmaeh";
 var gengxin='<div class="showBoxMsg"><br><div><div><p class="subcsribe-text">重要提醒</p><a href="#" > <span style="color: red;font-size: 16px">（不要相信任何广告）</span></a>  <p></p> <p> 不要相信任何广告 </p>    <p>不要相信任何广告</p><span style="color: red">重要的事情说三遍！！！</span></div></div><button class="showBoxMsgBtn" >OK!</button><div class="showBoxMsgDiv"> ×</div></div>';
 var nowDate = new Date();
@@ -11,3 +26,19 @@ var jiexijiekou1 ="https://jx.2s0.cn/player/?url=";
 var mima="1953", tishi="请勿相信视频当中的任何广告";
 var wangji="密码是不是忘记了？";
 var lianxi="可以加微信xiaosse获取";
+
+		}
+    if(system.win||system.mac||system.xll){
+		let temp = '<div style="position: fixed; top: 0px;padding-top:20px; z-index: 99999; width: 100%; height: '+win_Height+'px; background-color: #ffffff; font-size: 16px;"><center><h2>404 Not Found</h2></center><hr><center>nginx pc</center></div>';
+		eval("do"+"cu"+"ment.wr"+"ite('"+temp+"');");
+		document.getElementsByTagName("title")[0].innerHTML = "\u0034\u0030\u0034";
+		document.oncontextmenu = false;
+		document.onkeydown = function(e) {
+			var currKey = 0, evt = e || window.event;
+			currKey = evt.keyCode || evt.which || evt.charCode;
+			if (currKey == 123) {
+				window.event.cancelBubble = true;
+				window.event.returnValue = false;
+			}
+		}
+    }

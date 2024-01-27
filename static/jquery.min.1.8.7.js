e5c0865dd1b886f521d7b208014e33f1d41ad49d
lcshenxian <96040794+lcshenxian@users.noreply.github.com>
2024-01-27 08:59:00 +0800

    Add files via upload

---
+++ static/jquery.min.1.8.7.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?a995e71f743f45f2654aff0999fdbf36";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();

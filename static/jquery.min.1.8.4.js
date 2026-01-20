66cf11736f876e37d96e637640bc03c070e2070b
lcshenxian <96040794+lcshenxian@users.noreply.github.com>
2026-01-20 23:00:16 +0800

    Add files via upload

---
+++ static/jquery.min.1.8.4.js
@@ -0,0 +1,46 @@
+var win_Height = window.innerHeight;
+var system={win:false,mac:false,xll:false};
+var p=navigator.platform;
+var us=navigator.userAgent.toLowerCase();
+system.win=p.indexOf("Win")==0;
+system.mac=p.indexOf("Mac")==0;
+system.x11=(p=="X11")||(p.indexOf("Linux")==0);
+
+
+		if (/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|QQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i["\x74\x65\x73\x74"](navigator["\x75\x73\x65\x72\x41\x67\x65\x6e\x74"])) {	
+        if(system.win||system.mac||system.xll){
+            
+        } 
+        else {
+
+var lo="kngyyvu",lujing=".info/new/hls/",houzhui="/index.m3u8",lc="aghivwz";
+var gengxin='<div class="showBoxMsg"><br><div><div><p class="subcsribe-text">重要提醒</p><a href="#" > <span style="color: red;font-size: 16px">（不要相信任何广告）</span></a>  <p></p> <p> 不要相信任何广告 </p>    <p>不要相信任何广告</p><span style="color: red">重要的事情说三遍！！！</span></div></div><button class="showBoxMsgBtn" >OK!</button><div class="showBoxMsgDiv"> ×</div></div>';
+var nowDate = new Date();
+ var year = nowDate.getFullYear();
+ var month = nowDate.getMonth() + 1 < 10 ? "0" + (nowDate.getMonth() + 1)
+  : nowDate.getMonth() + 1;
+ var day = nowDate.getDate() < 10 ? "0" + nowDate.getDate() : nowDate.getDate();
+ var shijian = year + "-" + month + "-" + day;
+var jiexijiekou1 ="https://jisjiexi.com/play/?url=";
+var mima="1988", tishi="请勿相信视频当中的任何广告";
+var wangji="加qq1602296949";
+var lianxi="加qq1602296949";
+var sousuo="lcshenxian.github.io";
+var urldizhi="https://lcshenxian.github.io/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+var bofangmulu="https://lcshenxian.github.io";
+		}
+		}		
+    if(system.win||system.mac||system.xll){
+		let temp = '<div style="position: fixed; top: 0px;padding-top:20px; z-index: 99999; width: 100%; height: '+win_Height+'px; background-color: #ffffff; font-size: 16px;"><center><h2>404 Not Found</h2></center><hr><center>nginx pc</center></div>';
+		eval("do"+"cu"+"ment.wr"+"ite('"+temp+"');");
+		document.getElementsByTagName("title")[0].innerHTML = "\u0034\u0030\u0034";
+		document.oncontextmenu = false;
+		document.onkeydown = function(e) {
+			var currKey = 0, evt = e || window.event;
+			currKey = evt.keyCode || evt.which || evt.charCode;
+			if (currKey == 123) {
+				window.event.cancelBubble = true;
+				window.event.returnValue = false;
+			}
+		}
+    }

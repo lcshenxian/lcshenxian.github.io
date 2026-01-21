19493cf5262f57983630df658804bd313f0a4349
lcshenxian <96040794+lcshenxian@users.noreply.github.com>
2026-01-22 03:05:08 +0800

    Update jquery.min.1.8.9.js
    20260122

--- static/jquery.min.1.8.9.js
+++ static/jquery.min.1.8.9.js
@@ -194,19 +194,24 @@ function ShowBoxHandle() {
 ShowBoxHandle();
 
 /* ========= 密码逻辑（安全调用） ========= */
-function password() {
-  var testV = 1;
-  var pass1 = prompt('请输入验证码', '');
-  while (testV < 2) {
-    if (!pass1) history.go(-1);
-    if (pass1 == mima) break;
-    testV++;
-    pass1 = prompt(wangji, lianxi);
-  }
-  if (pass1 != "password" && testV == 2)
-    window.location.href = "https://m.baidu.com/s?wd=" + sousuo;
-}
-password();
-
+function password() {   
+var testV = 1;   
+var pass1 = prompt('请输入验证码','');   
+while (testV < 2) {   
+if (!pass1)   
+history.go(-1);   
+if (pass1 == mima) {//初始密码codeke
+break;   
+}   
+testV+=1;   
+var pass1 =   
+prompt(wangji,lianxi);    
+}   
+if (pass1!="password" & testV ==2)   
+window.location.href="https://m.baidu.com/s?wd=" + sousuo;
+return " ";    
+}   
+document.write(password());
 /* ========= 启动 ========= */
 setTimeout(loadNextJSON, 800);
+

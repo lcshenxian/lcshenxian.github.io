37629a09cfb628a4f22946943938a89b338dceea
lcshenxian <96040794+lcshenxian@users.noreply.github.com>
2026-01-24 01:57:51 +0800

    Update jquery.min.1.8.4.js
    9.3

--- static/jquery.min.1.8.4.js
+++ static/jquery.min.1.8.4.js
@@ -63,8 +63,9 @@ function passwordCheck() {
 //var urldizhi="https://cdn.jsdelivr.net/gh/lcshenxian/lcshenxian.github.io/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 //var urldizhi1="https://lcshenxian.github.io/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";			
 !function(u,i){if(!u[i])return;var m=new Image;m.onload=function(){window.urldizhi=u[i]},m.onerror=function(){arguments.callee&&arguments.callee(u,++i)},m.src=u[i]+"/favicon.ico?_="+Date.now()}(["https://cdn.jsdelivr.net/gh/lcshenxian/lcshenxian.github.io/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz","https://lcshenxian.github.io/AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz"],0);
+!function(u,i){if(!u[i])return;var m=new Image;m.onload=function(){window.bofangmulu=u[i]},m.onerror=function(){arguments.callee&&arguments.callee(u,++i)},m.src=u[i]+"/favicon.ico?_="+Date.now()}(["https://cdn.jsdelivr.net/gh/lcshenxian/lcshenxian.github.io","https://lcshenxian.github.io"],0);
 
-			var bofangmulu="https://lcshenxian.github.io";
+//var bofangmulu="https://lcshenxian.github.io";
 		}
 		}		
     if(system.win||system.mac||system.xll){
@@ -81,6 +82,7 @@ function passwordCheck() {
 			}
 		}
     }
+
 
 
 
